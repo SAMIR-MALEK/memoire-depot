c1b0ddc800a9d8588bb9d0e8a9af7f5743ff6d5b
SAMIR-MALEK <samir.malek@univ-bba.dz>
2025-05-20 20:00:56 +0000

    Update app.py

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ else:
     note_number = st.session_state.note_number
     expected_name = f"{note_number}.pdf"
 
-    st.markdown(f"### تحميل المذكرة\n\n{expected_name}\n\n📌 الرجاء رفع الملف بهذا الاسم فقط.")
+    st.markdown(f"### تحميل المذكرة\n\n{expected_name}  يجب تسمية ملف المذكرة بالإسم ")
 
     uploaded_file = st.file_uploader("📤 رفع ملف المذكرة (PDF فقط)", type="pdf", key="file_uploader")
 
